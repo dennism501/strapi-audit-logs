deb192daf9f31a6aef7ebc10af6144cdbc779beb
Dennis Mubamba <dennis@Denniss-MacBook-Pro.local>
2022-06-09 01:06:22 +0200

    Added env.sample variables

--- config/database.js
+++ config/database.js
@@ -1,6 +1,14 @@
 const path = require("path");
 
 module.exports = ({ env }) => ({
+
+  connection: {
+    client: 'sqlite',
+    connection: {
+      filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+    },
+    useNullAsDefault: true,
+    debug: false,
   //   connection: {
   //     client: env("DATABASE_CLIENT", "postgres"),
   //     connection: {
@@ -12,18 +20,18 @@ module.exports = ({ env }) => ({
   //     },
   // debug: false,
 
-  connection: {
-    client: "mysql",
-    connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 3306),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "strapi"),
-      password: env("DATABASE_PASSWORD", "strapi"),
-      ssl: {
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
-      },
-    },
-    debug: false,
+  // connection: {
+  //   client: "mysql",
+  //   connection: {
+  //     host: env("DATABASE_HOST", "127.0.0.1"),
+  //     port: env.int("DATABASE_PORT", 3306),
+  //     database: env("DATABASE_NAME", "strapi"),
+  //     user: env("DATABASE_USERNAME", "strapi"),
+  //     password: env("DATABASE_PASSWORD", "strapi"),
+  //     ssl: {
+  //       rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+  //     },
+  //   },
+  //   debug: false,
   },
 });
