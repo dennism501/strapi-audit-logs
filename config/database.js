359c171fb54c2269e6dc45620f28df1a22480961
Dennis Mubamba <dennis@Denniss-MacBook-Pro.local>
2022-06-09 01:01:23 +0200

    Added docker compose, postgres & mysql services

--- config/database.js
+++ config/database.js
@@ -1,11 +1,29 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = ({ env }) => ({
+  //   connection: {
+  //     client: env("DATABASE_CLIENT", "postgres"),
+  //     connection: {
+  //         host: env("DATABASE_HOST", "127.0.0.1"),
+  //         port: env.int("DATABASE_PORT", 5432),
+  //         database: env("DATABASE_NAME", "strapi"),
+  //         user: env("DATABASE_USERNAME", "strapi"),
+  //         password: env("DATABASE_PASSWORD", "strapi")
+  //     },
+  // debug: false,
+
   connection: {
-    client: 'sqlite',
+    client: "mysql",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 3306),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "strapi"),
+      password: env("DATABASE_PASSWORD", "strapi"),
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
